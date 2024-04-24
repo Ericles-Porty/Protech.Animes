5fe615cb3b66a6a8a659532fd8b6de7fcb3a05a8
Ericles <ericlesdsantos@gmail.com>
2024-04-24 15:42:48 -0300

    style: space between lines in entities

--- Protech.Animes.Domain/Entities/Anime.cs
+++ Protech.Animes.Domain/Entities/Anime.cs
@@ -3,8 +3,13 @@ namespace Protech.Animes.Domain.Entities;
 public class Anime
 {
     public int Id { get; set; }
+
     public required string Name { get; set; }
+
     public required string Summary { get; set; }
+
     public required Director Director { get; set; }
+
     public int DirectorId { get; set; }
+
 }
---
+++ Protech.Animes.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+namespace Protech.Animes.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Email { get; set; }
+
+    public required string Password { get; set; }
+
+    public required string Role { get; set; }
+
+}
