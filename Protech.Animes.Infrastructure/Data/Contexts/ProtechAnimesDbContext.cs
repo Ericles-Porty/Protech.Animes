6860ca0e716ce3b42a2c9a952a167e679f42d144
Ericles <ericlesdsantos@gmail.com>
2024-04-24 16:01:57 -0300

    feat: add user repository and interface

--- Protech.Animes.Infrastructure/Data/Contexts/ProtechAnimesDbContext.cs
+++ Protech.Animes.Infrastructure/Data/Contexts/ProtechAnimesDbContext.cs
@@ -10,7 +10,10 @@ public class ProtechAnimesDbContext : DbContext
     public ProtechAnimesDbContext(DbContextOptions<ProtechAnimesDbContext> options) : base(options) { }
 
     public DbSet<Anime> Animes { get; set; }
+
     public DbSet<Director> Directors { get; set; }
+
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Protech.Animes.Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+
+using Microsoft.EntityFrameworkCore;
+using Protech.Animes.Domain.Entities;
+using Protech.Animes.Infrastructure.Data.Contexts;
+using Protech.Animes.Infrastructure.Data.Repositories.Interfaces;
+
+namespace Protech.Animes.Infrastructure.Data.Repositories;
+
+public class UserRepository : IUserRepository
+{
+
+    private readonly ProtechAnimesDbContext _context;
+
+    public UserRepository(ProtechAnimesDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> CreateAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+
+    public async Task<User?> GetAsync(string email, string password)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == email && u.Password == password);
+
+        return user;
+    }
+
+}
