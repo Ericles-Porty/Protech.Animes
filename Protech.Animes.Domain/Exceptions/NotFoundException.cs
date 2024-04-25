a4f829e6d09f03a7e3d2e6adc06c6831b2d1ca15
Ericles <ericlesdsantos@gmail.com>
2024-04-24 22:49:08 -0300

    feat: update notfound exception

--- Protech.Animes.Domain/Exceptions/NotFoundException.cs
+++ Protech.Animes.Domain/Exceptions/NotFoundException.cs
@@ -2,6 +2,10 @@ namespace Protech.Animes.Domain.Exceptions;
 
 public class NotFoundException : Exception
 {
-    public NotFoundException() : base() { }
+    public NotFoundException() : base("Resource Not found") { }
+
     public NotFoundException(string message) : base(message) { }
+
+    public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
 }
