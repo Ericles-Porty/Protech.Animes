100d54889faca0e03b0027445e782efc69d92031
Ericles <ericlesdsantos@gmail.com>
2024-04-25 22:01:36 -0300

    feat: update anime service

--- Protech.Animes.Application/Interfaces/IAnimeService.cs
+++ Protech.Animes.Application/Interfaces/IAnimeService.cs
@@ -8,12 +8,18 @@ public interface IAnimeService
     Task<IEnumerable<AnimeDto>> GetAnimesPaginated(int page, int pageSize);
 
     Task<AnimeDto> GetAnime(int id);
+    Task<AnimeDto?> GetAnimeByName(string name);
 
     Task<AnimeDto> CreateAnime(AnimeDto animeDto);
+    Task<AnimeDto> CreateAnimeWithDirector(AnimeDto animeDto);
 
     Task<AnimeDto> UpdateAnime(int id, AnimeDto animeDto);
+    Task<AnimeDto> UpdateAnimeWithNewDirector(AnimeDto animeDto);
 
     Task<bool> DeleteAnime(int id);
+
+    Task<IEnumerable<AnimeDto>> GetAnimesByNamePattern(string name);
+    Task<IEnumerable<AnimeDto>> GetAnimesByNamePatternPaginated(string name, int page, int pageSize);
 
     Task<IEnumerable<AnimeDto>> GetAnimesByDirector(int directorId);
     Task<IEnumerable<AnimeDto>> GetAnimesByDirectorPaginated(int directorId, int page, int pageSize);
