760b8004cae4699f677ee1a3960d3ec38ea06d2a
Ericles <ericlesdsantos@gmail.com>
2024-04-25 23:15:29 -0300

    refact: if inversion on get anime by name usecase

--- Protech.Animes.Application/UseCases/Anime/GetAnimesByNameUseCase.cs
+++ Protech.Animes.Application/UseCases/Anime/GetAnimesByNameUseCase.cs
@@ -17,15 +17,13 @@ namespace Protech.Animes.Application.UseCases.Anime
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentException("Name cannot be null or empty");
 
-            if (page.HasValue && pageSize.HasValue)
-            {
-                if (page.Value <= 0 || pageSize.Value <= 0)
-                    throw new ArgumentException("Page and PageSize must be greater than 0");
+            if (!page.HasValue || !pageSize.HasValue)
+                return await _animeService.GetAnimesByNamePattern(name);
 
-                return await _animeService.GetAnimesByNamePatternPaginated(name, page.Value, pageSize.Value);
-            }
+            if (page.Value <= 0 || pageSize.Value <= 0)
+                throw new ArgumentException("Page and PageSize must be greater than 0");
 
-            return await _animeService.GetAnimesByNamePattern(name);
+            return await _animeService.GetAnimesByNamePatternPaginated(name, page.Value, pageSize.Value);
         }
     }
 }
