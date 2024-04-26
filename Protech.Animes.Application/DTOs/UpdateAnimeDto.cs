e8d422eed2da03e32b773c2b8c03676821cba171
Ericles <ericlesdsantos@gmail.com>
2024-04-25 22:00:46 -0300

    feat: add update anime dto

---
+++ Protech.Animes.Application/DTOs/UpdateAnimeDto.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Protech.Animes.Application.DTOs;
+
+public class UpdateAnimeDto
+{
+    [Key]
+    [Required(ErrorMessage = "Id is required")]
+    public required int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, ErrorMessage = "Name is too long")]
+    [MinLength(3, ErrorMessage = "Name is too short")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Summary is required")]
+    [StringLength(2000, ErrorMessage = "Summary is too long")]
+    [MinLength(3, ErrorMessage = "Summary is too short")]
+    public required string Summary { get; set; }
+
+    [Required(ErrorMessage = "DirectorId is required")]
+    public required int DirectorId { get; set; }
+
+    [Required(ErrorMessage = "Director name is required")]
+    [StringLength(100, ErrorMessage = "Director name is too long")]
+    [MinLength(3, ErrorMessage = "Director name is too short")]
+    public required string DirectorName { get; set; }
+}
